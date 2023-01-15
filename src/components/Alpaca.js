7928cc54bb5d752e123222c7220510af3f3dc078
Rizky Aria Mu'allim <74603462+RizkyAriaMuallim@users.noreply.github.com>
2023-01-15 23:23:17 +0700

    update

---
+++ src/components/Alpaca.js
@@ -0,0 +1,296 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { data, randomize } from "../utils/data";
+import mergeImages from "merge-images";
+
+const Alpaca = () => {
+	const [styleOptions, setStyleOptions] = useState({
+		...data.hair,
+	});
+	const [currentDataVal, setCurrentDataVal] = useState("hair");
+	const [alpacaStyle, setAlpacaStyle] = useState(
+		// hair: data.hair.default,
+		// ears: data.ears.default,
+		// eyes: data.eyes.default,
+		// leg: data.leg.default,
+		// mouth: data.mouth.default,
+		// neck: data.neck.default,
+		// accessories: data.accessories.headphone,
+		// backgrounds: data.backgrounds["yellow50.png"],
+		randomize()
+	);
+
+	function handleStyleOptionsRenser(item) {
+		setStyleOptions(data[item]);
+		setCurrentDataVal(item);
+	}
+
+	function handleStyleChose(item) {
+		setAlpacaStyle({
+			...alpacaStyle,
+			[currentDataVal]: data[currentDataVal][item],
+		});
+	}
+
+	function randomAlpaca() {
+		const res = randomize();
+		setAlpacaStyle({ ...res });
+	}
+
+	function download() {
+		mergeImages([
+			alpacaStyle.backgrounds,
+			alpacaStyle.accessories,
+			alpacaStyle.ears,
+			alpacaStyle.hair,
+			alpacaStyle.neck,
+			require("../assets/alpaca/nose.png"),
+			alpacaStyle.eyes,
+			alpacaStyle.mouth,
+			alpacaStyle.leg,
+		]).then((b64) => {
+			var a = document.createElement("a");
+			a.href = b64;
+			a.download = "Alpaca.png";
+			a.click();
+		});
+	}
+
+	return (
+		<StyledMain className="main">
+			<h1>ALPACA GENERATOR</h1>
+			<Container>
+				<StyledDivLeft>
+					<StyledDiv
+						style={{
+							background: `url(${alpacaStyle.backgrounds})`,
+						}}
+					>
+						<img
+							src={alpacaStyle.accessories}
+							alt="accessories"
+							className="accessories"
+						/>
+						<img
+							src={require("../assets/alpaca/nose.png")}
+							alt="nose"
+							className="nose"
+						/>
+						<img
+							src={alpacaStyle.ears}
+							alt="ear"
+							className="ears"
+						/>
+						<img
+							src={alpacaStyle.hair}
+							alt="hair"
+							className="hair"
+						/>
+						<img
+							src={alpacaStyle.eyes}
+							alt="eyes"
+							className="eyes"
+						/>
+						<img
+							src={alpacaStyle.mouth}
+							alt="mouth"
+							className="mouth"
+						/>
+						<img
+							src={alpacaStyle.neck}
+							alt="neck"
+							className="neck"
+						/>
+						<img src={alpacaStyle.leg} alt="leg" className="leg" />
+					</StyledDiv>
+
+					<ButtonsDiv>
+						<button onClick={randomAlpaca}>Random</button>
+						<button onClick={download}>Download</button>
+					</ButtonsDiv>
+				</StyledDivLeft>
+				<StyledDivRight>
+					<h3>ACCESSORIZE THE ALPACA'S</h3>
+
+					<Controls className="controls">
+						{Object.keys(data).map((item, id) => (
+							<button
+								onClick={() => handleStyleOptionsRenser(item)}
+								className={
+									item === currentDataVal ? "focus" : ""
+								}
+								key={id}
+							>
+								{item}
+							</button>
+						))}{" "}
+					</Controls>
+
+					<h3>STYLE</h3>
+					<Controls className="controls">
+						{Object.keys(styleOptions).map((item, id) => (
+							<button
+								onClick={() => handleStyleChose(item)}
+								className={
+									styleOptions[item] ===
+									alpacaStyle[currentDataVal]
+										? "focus"
+										: ""
+								}
+								key={id}
+							>
+								{item}
+							</button>
+						))}
+					</Controls>
+				</StyledDivRight>
+			</Container>
+		</StyledMain>
+	);
+};
+
+export default Alpaca;
+
+const StyledMain = styled.main`
+	padding: 32px;
+	background-color: #eeecee;
+	text-align: center;
+	height: 100%;
+
+	h1 {
+		font-size: 2.5rem;
+		margin-bottom: 32px;
+	}
+`;
+
+const Container = styled.div`
+	display: grid;
+	grid-template-columns: 1fr 1fr;
+
+	@media (max-width: 800px) {
+		grid-template-columns: 1fr;
+	}
+`;
+const StyledDiv = styled.div`
+	position: relative;
+	width: 500px;
+	min-height: 410px;
+	margin: 0 auto;
+	img {
+		width: 400px;
+	}
+
+	@media (max-width: 750px) {
+		width: 250px;
+		min-height: 212px;
+		margin: 0 auto;
+		img {
+			width: 200px;
+		}
+	}
+	.accessories {
+		position: absolute;
+		top: 11px;
+		left: 11px;
+		z-index: 4;
+	}
+	.ears {
+		position: absolute;
+		top: 12px;
+		left: 13px;
+	}
+
+	.hair {
+		position: absolute;
+		top: 11px;
+		left: 11px;
+		z-index: 2;
+	}
+	.neck {
+		position: absolute;
+		top: 11px;
+		left: 11px;
+		z-index: 1;
+	}
+
+	.nose {
+		position: absolute;
+		top: 11px;
+		left: 11px;
+		z-index: 3;
+	}
+
+	.eyes {
+		position: absolute;
+		top: 11px;
+		left: 11px;
+		z-index: 3;
+	}
+
+	.mouth {
+		position: absolute;
+		top: 11px;
+		left: 11px;
+		z-index: 3;
+	}
+	.leg {
+		position: absolute;
+		bottom: 0;
+		right: 0;
+		z-index: 3;
+	}
+`;
+
+const StyledDivLeft = styled.div`
+	justify-self: center;
+`;
+const StyledDivRight = styled.div`
+	justify-self: center;
+	h3 {
+		font-size: 1.5rem;
+		margin: 8px 0 16px 0;
+		@media (max-width: 800px) {
+			margin: 32px 0;
+		}
+	}
+`;
+const Controls = styled.div`
+	display: flex;
+	flex-wrap: wrap;
+	gap: 8px;
+	max-width: 400px;
+	button {
+		border: 0;
+		outline: 0;
+		border: 1px solid blue;
+		border-radius: 32px;
+		padding: 8px 32px;
+		background-color: none;
+		flex: 1;
+		font-size: 1rem;
+		color: #2a5ca7;
+		cursor: pointer;
+	}
+
+	button:hover {
+		opacity: 0.6;
+		border: 1px solid #2a5ca7;
+		color: #2a5ca7;
+	}
+
+	button.focus {
+		background-color: #2a5ca7;
+		color: white;
+	}
+`;
+
+const ButtonsDiv = styled.div`
+	margin-top: 64px;
+	display: flex;
+	gap: 16px;
+	justify-content: center;
+
+	button {
+		padding: 16px;
+	}
+`;
---
+++ src/utils/data.js
@@ -0,0 +1,88 @@
+export const data = {
+	hair: {
+		default: require("../assets/alpaca/hair/default.png"),
+		curls: require("../assets/alpaca/hair/curls.png"),
+		short: require("../assets/alpaca/hair/short.png"),
+		bang: require("../assets/alpaca/hair/bang.png"),
+		elegant: require("../assets/alpaca/hair/elegant.png"),
+		quiff: require("../assets/alpaca/hair/quiff.png"),
+	},
+	ears: {
+		default: require("../assets/alpaca/ears/default.png"),
+		"tilt backward": require("../assets/alpaca/ears/tilt-backward.png"),
+		"tilt forward": require("../assets/alpaca/ears/tilt-forward.png"),
+	},
+	eyes: {
+		default: require("../assets/alpaca/eyes/default.png"),
+		angry: require("../assets/alpaca/eyes/angry.png"),
+		naughty: require("../assets/alpaca/eyes/naughty.png"),
+		panda: require("../assets/alpaca/eyes/panda.png"),
+		smart: require("../assets/alpaca/eyes/smart.png"),
+		star: require("../assets/alpaca/eyes/star.png"),
+	},
+	leg: {
+		"bubble tea": require("../assets/alpaca/leg/bubble-tea.png"),
+		cookie: require("../assets/alpaca/leg/cookie.png"),
+		default: require("../assets/alpaca/leg/default.png"),
+		"game console": require("../assets/alpaca/leg/game-console.png"),
+		"tilt backward": require("../assets/alpaca/leg/tilt-backward.png"),
+		"tilt forward": require("../assets/alpaca/leg/tilt-forward.png"),
+	},
+	mouth: {
+		astonished: require("../assets/alpaca/mouth/astonished.png"),
+		default: require("../assets/alpaca/mouth/default.png"),
+		eating: require("../assets/alpaca/mouth/eating.png"),
+		laugh: require("../assets/alpaca/mouth/laugh.png"),
+		tongue: require("../assets/alpaca/mouth/tongue.png"),
+	},
+	neck: {
+		"bend backword": require("../assets/alpaca/neck/bend-backward.png"),
+		"bend forward": require("../assets/alpaca/neck/bend-forward.png"),
+		default: require("../assets/alpaca/neck/default.png"),
+		thick: require("../assets/alpaca/neck/thick.png"),
+	},
+	accessories: {
+		earings: require("../assets/alpaca/accessories/earings.png"),
+		flower: require("../assets/alpaca/accessories/flower.png"),
+		glasses: require("../assets/alpaca/accessories/glasses.png"),
+		headphone: require("../assets/alpaca/accessories/headphone.png"),
+	},
+	backgrounds: {
+		"blue50.png": require("../assets/alpaca/backgrounds/blue50.png"),
+		"blue60.png": require("../assets/alpaca/backgrounds/blue60.png"),
+		"blue70.png": require("../assets/alpaca/backgrounds/blue70.png"),
+		"darkblue30.png": require("../assets/alpaca/backgrounds/darkblue30.png"),
+		"darkblue50.png": require("../assets/alpaca/backgrounds/darkblue50.png"),
+		"darkblue70.png": require("../assets/alpaca/backgrounds/darkblue70.png"),
+		"green50.png": require("../assets/alpaca/backgrounds/green50.png"),
+		"green60.png": require("../assets/alpaca/backgrounds/green60.png"),
+		"green70.png": require("../assets/alpaca/backgrounds/green70.png"),
+		"grey40.png": require("../assets/alpaca/backgrounds/grey40.png"),
+		"grey70.png": require("../assets/alpaca/backgrounds/grey70.png"),
+		"grey80.png": require("../assets/alpaca/backgrounds/grey80.png"),
+		"red50.png": require("../assets/alpaca/backgrounds/red50.png"),
+		"red60.png": require("../assets/alpaca/backgrounds/red60.png"),
+		"red70.png": require("../assets/alpaca/backgrounds/red70.png"),
+		"yellow50.png": require("../assets/alpaca/backgrounds/yellow50.png"),
+		"yellow60.png": require("../assets/alpaca/backgrounds/yellow60.png"),
+		"yellow70.png": require("../assets/alpaca/backgrounds/yellow70.png"),
+	},
+};
+
+function randomObjVal(obj) {
+	var keys = Object.keys(obj);
+	return obj[keys[(keys.length * Math.random()) << 0]];
+}
+export function randomize() {
+	const res = {
+		hair: randomObjVal(data.hair),
+		ears: randomObjVal(data.ears),
+		eyes: randomObjVal(data.eyes),
+		leg: randomObjVal(data.leg),
+		mouth: randomObjVal(data.mouth),
+		neck: randomObjVal(data.neck),
+		accessories: randomObjVal(data.accessories),
+		backgrounds: randomObjVal(data.backgrounds),
+	};
+	return res;
+}
