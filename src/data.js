03618df71d9e71ef2da2bd20592e6647cb581c91
Rizky Aria Mu'allim <74603462+RizkyAriaMuallim@users.noreply.github.com>
2023-01-15 22:57:07 +0700

    update

---
+++ src/data.js
@@ -0,0 +1,244 @@
+export const accessories = [
+	{
+		name: "earings",
+		url: "./alpaca/accessories/earings.png",
+	},
+	{
+		name: "flower",
+		url: "./alpaca/accessories/flower.png",
+	},
+	{
+		name: "glasses",
+		url: "./alpaca/accessories/glasses.png",
+	},
+	{
+		name: "headphone",
+		url: "./alpaca/accessories/headphone.png",
+		default: true,
+	},
+];
+
+export const backgrounds = [
+	{
+		name: "blue50",
+		url: "./alpaca/backgrounds/blue50.png",
+		default: true,
+	},
+	{
+		name: "blue60",
+		url: "./alpaca/backgrounds/blue60.png",
+	},
+	{
+		name: "blue70",
+		url: "./alpaca/backgrounds/blue70.png",
+	},
+	{
+		name: "darkblue30",
+		url: "./alpaca/backgrounds/darkblue30.png",
+	},
+	{
+		name: "darkblue50",
+		url: "./alpaca/backgrounds/darkblue50.png",
+	},
+	{
+		name: "darkblue70",
+		url: "./alpaca/backgrounds/darkblue70.png",
+	},
+	{
+		name: "green50",
+		url: "./alpaca/backgrounds/green50.png",
+	},
+	{
+		name: "green60",
+		url: "./alpaca/backgrounds/green60.png",
+	},
+	{
+		name: "green70",
+		url: "./alpaca/backgrounds/green70.png",
+	},
+	{
+		name: "grey40",
+		url: "./alpaca/backgrounds/grey40.png",
+	},
+	{
+		name: "grey70",
+		url: "./alpaca/backgrounds/grey70.png",
+	},
+	{
+		name: "grey80",
+		url: "./alpaca/backgrounds/grey80.png",
+	},
+	{
+		name: "red50",
+		url: "./alpaca/backgrounds/red50.png",
+	},
+	{
+		name: "red60",
+		url: "./alpaca/backgrounds/red60.png",
+	},
+	{
+		name: "red70",
+		url: "./alpaca/backgrounds/red70.png",
+	},
+	{
+		name: "yellow50",
+		url: "./alpaca/backgrounds/yellow50.png",
+	},
+	{
+		name: "yellow60",
+		url: "./alpaca/backgrounds/yellow60.png",
+	},
+	{
+		name: "yellow70",
+		url: "./alpaca/backgrounds/yellow70.png",
+	},
+];
+
+export const ears = [
+	{
+		name: "default",
+		url: "./alpaca/ears/default.png",
+		default: true,
+	},
+	{
+		name: "tilt-backward",
+		url: "./alpaca/ears/tilt-backward.png",
+	},
+	{
+		name: "tilt-forward",
+		url: "./alpaca/ears/tilt-forward.png",
+	},
+];
+export const eyes = [
+	{
+		name: "default",
+		url: "./alpaca/eyes/default.png",
+		default: true,
+	},
+	{
+		name: "angry",
+		url: "./alpaca/eyes/angry.png",
+	},
+	{
+		name: "naughty",
+		url: "./alpaca/eyes/naughty.png",
+	},
+	{
+		name: "panda",
+		url: "./alpaca/eyes/panda.png",
+	},
+	{
+		name: "smart",
+		url: "./alpaca/eyes/smart.png",
+	},
+	{
+		name: "star",
+		url: "./alpaca/eyes/star.png",
+	},
+];
+
+export const hair = [
+	{
+		name: "default",
+		url: "./alpaca/hair/default.png",
+		default: true,
+	},
+	{
+		name: "bang",
+		url: "./alpaca/hair/bang.png",
+	},
+
+	{
+		name: "curls",
+		url: "./alpaca/hair/curls.png",
+	},
+	{
+		name: "elegant",
+		url: "./alpaca/hair/elegant.png",
+	},
+	{
+		name: "fancy",
+		url: "./alpaca/hair/fancy.png",
+	},
+
+	{
+		name: "quiff",
+		url: "./alpaca/hair/quiff.png",
+	},
+	{
+		name: "short",
+		url: "./alpaca/hair/short.png",
+	},
+];
+
+export const leg = [
+	{
+		name: "default",
+		url: "./alpaca/leg/default.png",
+		default: true,
+	},
+	{
+		name: "bubble-tea",
+		url: "./alpaca/leg/bubble-tea.png",
+	},
+	{
+		name: "cookie",
+		url: "./alpaca/leg/cookie.png",
+	},
+	{
+		name: "game-console",
+		url: "./alpaca/leg/game-console.png",
+	},
+	{
+		name: "tilt-backward",
+		url: "./alpaca/leg/tilt-backward.png",
+	},
+	{
+		name: "tilt-forward",
+		url: "./alpaca/leg/tilt-forward.png",
+	},
+];
+
+export const mouth = [
+	{
+		name: "default",
+		url: "./alpaca/mouth/default.png",
+		default: true,
+	},
+	{
+		name: "astonished",
+		url: "./alpaca/mouth/astonished.png",
+	},
+	{
+		name: "eating",
+		url: "./alpaca/mouth/eating.png",
+	},
+	{
+		name: "laugh",
+		url: "./alpaca/mouth/laugh.png",
+	},
+	{
+		name: "tongue",
+		url: "./alpaca/mouth/tongue.png",
+	},
+];
+
+export const neck = [
+	{
+		name: "default",
+		url: "./alpaca/neck/default.png",
+		default: true,
+	},
+	{
+		name: "bend-backward",
+		url: "./alpaca/neck/bend-backward.png",
+	},
+	{
+		name: "bend-forward",
+		url: "./alpaca/neck/bend-forward.png",
+	},
+	{
+		name: "thick",
+		url: "./alpaca/neck/thick.png",
+	},
+];
